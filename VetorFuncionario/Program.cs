f89b483c8dfc8586741122b93da0596432c29585
luana-novais <luana_novais@outlook.com.br>
2025-08-30 12:09:10 -0300

    Exercicio Vetor

---
+++ VetorFuncionario/Program.cs
@@ -0,0 +1,32 @@
+﻿using VetorFuncionario;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Funcionario [] VetFuncionario = new Funcionario[3];
+        double soma = 0;
+        
+        for (int i = 0; i < VetFuncionario.Length; i++)
+        {
+            VetFuncionario[i] = new Funcionario();
+
+            Console.WriteLine("Digite o código do fucionario");
+            VetFuncionario[i].codigo = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o nome: ");
+            VetFuncionario[i].nome = Console.ReadLine();
+
+            Console.Write("Digite o salario: ");
+            VetFuncionario[i].salario = Convert.ToDouble(Console.ReadLine());
+        }
+
+        for (int i = 0; i < VetFuncionario.Length; i++)
+        {
+            VetFuncionario[i].MostrarAtributos();
+            soma += VetFuncionario[i].salario;
+        }
+
+        Console.WriteLine("Total de sálarios: " + soma);
+
+    }
+}
