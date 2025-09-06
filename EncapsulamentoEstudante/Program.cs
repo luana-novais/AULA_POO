2b9ac51100b3d8dfa58a0f578dccd13fbc540c43
Luana Novais <luana_novais@outlook.com.br>
2025-09-06 12:52:23 -0300

    Aula 06-09

---
+++ EncapsulamentoEstudante/Program.cs
@@ -0,0 +1,15 @@
+﻿using EncapsulamentoEstudante;
+
+Estudante e1 = new Estudante();
+e1.Nome = "Luana";
+e1.Media = 5;
+
+Estudante e2 = new Estudante();
+e2.Nome = "Gustavo";
+e2.Media = 10;
+
+e1.ExibirDetalhes();
+e1.ExibirInformacoes();
+
+e2.ExibirDetalhes();
+e2.ExibirInformacoes();
---
+++ EncapsulamentoProduto/Program.cs
@@ -0,0 +1,9 @@
+﻿using EncapsulamentoProduto;
+
+Produto p = new Produto();
+p.Codigo = 1;
+p.Nome = "mouse";
+p.Preco = 50;
+
+if (p.Preco > 10)
+    Console.WriteLine("Preço alto " + p.Preco);
